713d2d0c3e971e6b913188020bddce4f92b43e09
Lasen Punyawardana <lpunyawardana@gmail.com>
2025-01-08 15:09:52 +0530

    Initial commit

---
+++ src/main/java/org/example/attendancesystembe/AttendanceSystemBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.attendancesystembe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceSystemBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceSystemBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/attendancesystembe/AttendanceSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.attendancesystembe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceSystemBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
